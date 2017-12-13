04198c4caeffa2cf036a97a115bf12d5b98f7205
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2017-12-13 10:56:25 +0000

    started day 13

---
+++ src/test/kotlin/com/pauldambra/adventofcode2017/day13/FirewallLayerTests.kt
@@ -0,0 +1,49 @@
+package com.pauldambra.adventofcode2017.day13
+
+import com.winterbe.expekt.expect
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.it
+
+object FirewallLayerTests : Spek({
+    it("can create a layer") {
+        expect(Layer("0: 3")).to.equal(Layer(0, 3))
+    }
+    it("can create two consecutive layers") {
+        val layers: List<FirewallLayer> = Layer.parse(
+                "0: 3\n" +
+                "1: 2")
+        expect(layers).to.equal(listOf(Layer(0, 3), Layer(1, 2)))
+    }
+    it("can create non-consecutive layers") {
+        val layers: List<FirewallLayer> = Layer.parse(
+                "0: 4\n" +
+                "3: 3")
+        expect(layers).to.equal(listOf(Layer(0, 4), EmptyLayer(1), EmptyLayer(2), Layer(3, 3)))
+    }
+})
+
+interface FirewallLayer
+
+data class EmptyLayer(val depth: Int) : FirewallLayer
+
+data class Layer(val depth: Int, val range: Int) : FirewallLayer {
+    companion object {
+        fun parse(description: String): List<FirewallLayer> {
+            val providedLayers = description
+                    .split("\n")
+                    .map { Layer(it) }
+                    .associateBy( { it.depth }, {it} )
+
+            val depthsRange = providedLayers.keys.min()!!.rangeTo(providedLayers.keys.max()!!)
+
+            return depthsRange.fold(emptyList()) { layers, depth ->
+                layers + providedLayers.getOrDefault(depth, EmptyLayer(depth))
+            }
+        }
+    }
+
+    constructor(description: String) : this(
+            description.split(": ")[0].toInt(),
+            description.split(": ")[1].toInt())
+
+}
