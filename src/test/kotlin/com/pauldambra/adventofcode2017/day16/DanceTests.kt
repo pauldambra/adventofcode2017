7660f013259f8184e46bd8ddb95ebfd4c6280aec
pauldambra <paul.dambra@gmail.com>
2017-12-16 13:31:50 +0000

    day 16 part 2 solution takes about 200 seconds

--- src/test/kotlin/com/pauldambra/adventofcode2017/day16/DanceTests.kt
+++ src/test/kotlin/com/pauldambra/adventofcode2017/day16/DanceTests.kt
@@ -81,11 +81,20 @@ data class Group(var state: MutableList<Char>) {
     private val seenDances = mutableMapOf<List<Char>, List<Char>>()
 
     fun dance(moves: String, repeats: Int) {
+//        var startTime = System.currentTimeMillis()
         (1..repeats).forEach { n ->
+
+//            if (n % 1_000_000 == 0) {
+//                val elapsed = System.currentTimeMillis() - startTime
+//                val expectedCompletion = ((elapsed.toDouble() / 1_000_000) / 1000) * 1_000_000_000
+//                println("$n: took $elapsed milliseconds for a million. so about $expectedCompletion seconds total")
+//                startTime = System.currentTimeMillis()
+//            }
+
             val start = state.toList()
 
             //if we've seen a start before then we don't need to do the dance again
-            if(seenDances.containsKey(start)) {
+            if (seenDances.containsKey(start)) {
                 state = seenDances[start]!!.toMutableList()
             } else {
                 moves.split(",").forEach { singleDanceStep(it) }
@@ -95,22 +104,13 @@ data class Group(var state: MutableList<Char>) {
         }
     }
 
-    val seenSteps = mutableMapOf<List<Char>, List<Char>>()
-
     fun singleDanceStep(danceMove: String) {
-
-        val key = state.toList()+danceMove.toCharArray().toList()
-        if (seenSteps.containsKey(key)) {
-            state = seenSteps[key]!!.toMutableList()
-        } else {
-            val type = danceMove.first()
-            when (type) {
-                's' -> spin(danceMove)
-                'x' -> exchange(danceMove)
-                'p' -> partner(danceMove)
-                else -> throw Exception("unknown dance move $type")
-            }
-            seenSteps.put(key, state.toList())
+        val type = danceMove.first()
+        when (type) {
+            's' -> spin(danceMove)
+            'x' -> exchange(danceMove)
+            'p' -> partner(danceMove)
+            else -> throw Exception("unknown dance move $type")
         }
     }
 
