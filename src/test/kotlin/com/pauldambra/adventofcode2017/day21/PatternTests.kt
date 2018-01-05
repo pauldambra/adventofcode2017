d1c217a5631caffced11fbb0d0c654be275148e7
pauldambra <paul.dambra@gmail.com>
2018-01-05 23:24:18 +0000

    add day 21 part one solution

--- src/test/kotlin/com/pauldambra/adventofcode2017/day21/PatternTests.kt
+++ src/test/kotlin/com/pauldambra/adventofcode2017/day21/PatternTests.kt
@@ -5,250 +5,298 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.it
 
+val startingGrid =
+  ".#.\n" +
+    "..#\n" +
+    "###"
 
-object PatternTests : Spek(
+val line = ".#./..#/###"
+
+object PixelArtTests : Spek(
   {
-      it("can report its size") {
-          val pattern = Pattern.parse(".#.\n" +
-                                        "..#\n" +
-                                        "###")
-          expect(pattern.size).to.equal(3)
+      it("can convert grids to lines for the pixel art") {
+          expect(startingGrid.convertToLine()).to.equal(line)
       }
 
-      context("splitting patterns") {
-          it("can split a 2x2 square into 1 split") {
-              val p = Pattern.parse("12\n34")
-              val s = p.split()
-              expect(s.count()).to.equal(1)
+      it("can replace grids by matching against patterns") {
+          val replacementPattern = "#..#/..../..../#..#"
+          val patterns = mapOf(line to replacementPattern)
+          expect(startingGrid.replaceFrom(patterns)).to.equal(replacementPattern)
+      }
+
+      it("can rotate grids") {
+          val rotatedLine = "#../#.#/##."
+          expect(line.rotate()).to.equal(rotatedLine)
+      }
+
+      it("can generate all the possible rotations of the starting grid") {
+          val expected = listOf("#../#.#/##.",
+                                "###/#../.#.",
+                                ".##/#.#/..#",
+                                ".#./..#/###",
+                                ".#./#../###",
+                                "###/..#/.#.",
+                                "..#/#.#/.##",
+                                "##./#.#/#..")
+
+          val newLineDelimited = startingGrid.generateRotatedGrids()
+          expected.forEach {
+              expect(newLineDelimited.contains(it)).to.be.`true`
           }
 
-          it("can split a 4x4 square into 4 splits") {
-              val p = Pattern.parse("1234\n" +
-                                      "5678\n" +
-                                      "9abc\n" +
-                                      "defg")
-              val s = p.split()
-              val expected = listOf(
-                Pattern.parse("12\n56"),
-                Pattern.parse("34\n78"),
-                Pattern.parse("9a\nde"),
-                Pattern.parse("bc\nfg")
-              )
-              expect(s).to.equal(expected)
-          }
-
-          it("can split a 3x3 square into 1 split") {
-              val threeByThree = "123\n" +
-                "456\n" +
-                "789"
-              val p = Pattern.parse(threeByThree)
-              val s = p.split()
-              expect(s.count()).to.equal(1)
-              expect(s).to.equal(listOf(Pattern.parse(threeByThree)))
-          }
-
-          it("can split a 6x6 square into 4 splits") {
-              val p = Pattern.parse("123456\n" +
-                                      "789abc\n" +
-                                      "defghi\n" +
-                                      "jklmno\n" +
-                                      "pqrstu\n" +
-                                      "vwxyz!"
-              )
-              val s = p.split()
-              val expected = listOf(
-                Pattern.parse("123\n789\ndef"),
-                Pattern.parse("456\nabc\nghi"),
-                Pattern.parse("jkl\npqr\nvwx"),
-                Pattern.parse("mno\nstu\nyz!")
-              )
-              s.withIndex().forEach {
-                  expect(it.value).to.equal(expected[it.index])
-              }
-          }
-
-          /*
-          2x2 splits to 1 size 2
-          3x3 splits to 1 size 3
-
-          4x4 splits to 4 size 2
-          6x6 splits to 9 size 2
-          8x8 splits to 12 size 2
-          9x9 splits to 9 size 3
-          10x10 splits to 25 size 2
-          12x12 splits to 30 size 2
-
-            */
-
-          it("can combine a 4x4 split") {
-              val split = listOf(
-                Pattern.parse("12\n78"),
-                Pattern.parse("34\n9a"),
-                Pattern.parse("de\njk"),
-                Pattern.parse("fg\nlm")
-              )
-              val combinedPattern = Pattern.combine(split)
-              expect(combinedPattern).to.equal(Pattern.parse(
-                "1234\n" +
-                  "789a\n" +
-                  "defg\n" +
-                  "jklm"
-              ))
-          }
-
-          it("can combine a 6x6 split") {
-              val split = listOf(
-                Pattern.parse("12\n78"),
-                Pattern.parse("34\n9a"),
-                Pattern.parse("56\nbc"),
-                Pattern.parse("de\njk"),
-                Pattern.parse("fg\nlm"),
-                Pattern.parse("hi\nno"),
-                Pattern.parse("pq\nvw"),
-                Pattern.parse("rs\nxy"),
-                Pattern.parse("tu\nz!")
-              )
-              val combinedPattern = Pattern.combine(split)
-              expect(combinedPattern).to.equal(Pattern.parse(
-                "123456\n" +
-                  "789abc\n" +
-                  "defghi\n" +
-                  "jklmno\n" +
-                  "pqrstu\n" +
-                  "vwxyz!"
-              ))
-          }
-
-          it("can combine a 8x8 split") {
-              val split = listOf(
-                Pattern.parse("12\n9a"),
-                Pattern.parse("34\nbc"),
-                Pattern.parse("56\nde"),
-                Pattern.parse("78\nfg"),
-                Pattern.parse("hi\npq"),
-                Pattern.parse("jk\nrs"),
-                Pattern.parse("lm\ntu"),
-                Pattern.parse("no\nvw"),
-                Pattern.parse("xy\n^&"),
-                Pattern.parse("z!\n*("),
-                Pattern.parse("@£\n)_"),
-                Pattern.parse("$%\n+{")
-              )
-              val combinedPattern = Pattern.combine(split)
-              expect(combinedPattern).to.equal(Pattern.parse(
-                "12345678\n" +
-                  "9abcdefg\n" +
-                  "hijklmno\n" +
-                  "pqrstuvw\n" +
-                  "xyz!@£$%\n" +
-                  "^&*()_+{"
-              ))
+          val slashDelimited = line.generateRotatedGrids()
+          expected.forEach {
+              expect(slashDelimited.contains(it)).to.be.`true`
           }
       }
+
+      context("generate art") {
+          it("can take the first step") {
+              val rules = mapOf(
+                "../.#" to "##./#../...",
+                ".#./..#/###" to "#..#/..../..../#..#"
+              )
+
+              val final = startingGrid.generateArt(rules, 2)
+              expect(final).to.equal("##.##./#..#../....../##.##./#..#../......")
+
+              expect(final.pixelsOn()).to.equal(12)
+          }
+
+          it("can generate art for the puzzle input") {
+              val rules = this::class.java
+                .getResource("/day21.txt")
+                .readText()
+                .split("\n")
+                .map { it.split(" => ") }
+                .map { Pair(it[0], it[1]) }
+                .toMap()
+
+              val final = startingGrid.generateArt(rules, 5)
+              expect(final.pixelsOn()).not.to.equal(10)
+              println("day 21 part 1: there are ${final.pixelsOn()} pixels on after 5 iterations")
+          }
+      }
+
+      context("can match flipped and rotated patterns") {
+          val replacementPattern = "#..#/..../..../#..#"
+          val patterns = mapOf(Pair(line, replacementPattern))
+          it("can match original pattern") {
+              val unchanged =
+                ".#.\n" +
+                  "..#\n" +
+                  "###"
+
+
+              expect(unchanged.replaceFrom(patterns)).to.equal(replacementPattern)
+          }
+          it("can match flipped pattern") {
+              val flipped =
+                ".#.\n" +
+                  "#..\n" +
+                  "###"
+
+              expect(flipped.replaceFrom(patterns)).to.equal(replacementPattern)
+          }
+          it("can match rotated right pattern") {
+              val rotatedRight =
+                "#..\n" +
+                  "#.#\n" +
+                  "##."
+
+              expect(rotatedRight.replaceFrom(patterns)).to.equal(replacementPattern)
+          }
+          it("can match upside down pattern") {
+              val upsideDown =
+                "###\n" +
+                  "..#\n" +
+                  ".#."
+
+              expect(upsideDown.replaceFrom(patterns)).to.equal(replacementPattern)
+          }
+      }
+
+      context("can split grids") {
+          it("can split the starting grid") {
+              val splat = startingGrid.splitGrid()
+              expect(splat).to.have.elements(line)
+          }
+
+          it("can split a larger grid") {
+              val splat = ("#..#\n" +
+                "....\n" +
+                "....\n" +
+                "#..#").splitGrid()
+              expect(splat).to.have.elements("#./..", ".#/..", "../#.", "../.#")
+          }
+
+          it("can split an even larger grid") {
+              val splat = (
+                "##.##.\n" +
+                  "#..#..\n" +
+                  "......\n" +
+                  "##.##.\n" +
+                  "#..#..\n" +
+                  "......").splitGrid()
+              expect(splat).to.have.elements(
+                "##/#.",
+                ".#/.#",
+                "#./..",
+                "../##",
+                "../.#",
+                "../#.",
+                "#./..",
+                ".#/..",
+                "../.."
+              )
+          }
+
+          it("can correctly split a 9x9 grid") {
+              val start = "...#....#/.###.#.../..#..#..#/........./..##..#.#/...#.##.."
+              val expected = listOf(
+                ".../.##/..#",
+                "#../#.#/..#",
+                "..#/.../..#",
+                ".../..#/...",
+                ".../#../#.#",
+                ".../#.#/#.."
+              )
+
+              val actual = start.splitGrid()
+
+              expect(actual).to.equal(expected)
+          }
+      }
+
   })
 
+private fun String.pixelsOn() = this.count { it == '#' }
 
-class Pattern constructor(private val patternGrid: List<List<String>>) {
-    class GridMustBeDivisibleByTwoOrThree(size: Int) : Throwable("grid size is $size")
+private fun String.generateArt(rules: Map<String, String>, iterations: Int): String {
+    var next = this.convertToLine()
+    repeat(iterations) {
+        val splat = next.splitGrid()
+//        println("splats to $splat")
 
-    companion object {
-        fun parse(description: String): Pattern {
-            return Pattern(description.split("\n").map { it.toCharArray().map { it.toString() } })
+        val enhancedSquares = splat.map { it.replaceFrom(rules) }.toList()
 
-        }
+        next = combineEnhancedSquares(enhancedSquares)
 
-        private fun splitSizeForGrid(gridSize: Int)
-          = when {
-            gridSize % 3 == 0 -> 3
-            gridSize % 2 == 0 -> 2
-            else              -> throw GridMustBeDivisibleByTwoOrThree(gridSize)
-        }
-
-        private fun fangleChunks(chunks: List<List<List<String>>>, splitSize: Int): MutableList<List<List<String>>> {
-            val x = mutableListOf<List<List<String>>>()
-            for (i in 0 until chunks.size step splitSize) {
-                println("when i is $i")
-                val a = chunks[i]
-                val b = chunks[i + 1]
-                println("a is $a")
-                println("b is $b")
-
-                var c: List<List<String>>? = null
-                if (splitSize == 3) {
-                    c = chunks[i + 2]
-                    println("c is $c")
-                }
-
-                (0 until a.size).mapTo(x) {
-                    val grid = listOf(a[it], b[it])
-                    if (c == null) grid else grid + listOf(c[it])
-                }
-            }
-            return x
-        }
-
-        fun combine(patterns: List<Pattern>): Pattern {
-            if (patterns.size == 1) {
-                return patterns.single()
-            }
-
-            val splitSize = splitSizeForGrid(patterns.size)
-            println("combining a split with size $splitSize")
-
-            val x = mutableListOf<List<String>>()
-            for (i in 0 until patterns.size step splitSize) {
-                println("patterns[i] size is ${patterns[i].size}")
-                println("there are ${patterns.size} patterns")
-                val a = patterns[i].patternGrid
-                val b = patterns[i + 1].patternGrid
-
-                var c: List<List<String>>? = null
-                if (splitSize == 3) {
-                    c = patterns[i + 2].patternGrid
-                    println("c is $c")
-                }
-
-                (0 until a.size).mapTo(x) {
-                    val grid = a[it] + b[it]
-                    if (c == null) grid else grid + c[it]
-                }
-            }
-            return Pattern(x.toList())
-        }
-
-    }
-
-    //assumes it's a square
-    val size = patternGrid.count()
-
-    fun split(): List<Pattern> {
-
-        val splitSize = splitSizeForGrid(patternGrid[0].size)
-
-        val chunks = patternGrid.map { it.chunked(splitSize) }
-
-        return fangleChunks(chunks, splitSize).map(::Pattern)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Pattern
-
-        if (patternGrid != other.patternGrid) return false
-
-        return true
+//        println("after replacement we have $next")
     }
 
 
-    override fun hashCode(): Int {
-        return patternGrid.hashCode()
-    }
-
-    override fun toString(): String {
-        return "Pattern(patternGrid=${patternGrid.joinToString("/")})"
-    }
-
-
+    return next
 }
+
+private fun combineEnhancedSquares(enhancedSquares: List<String>): String {
+    return if (enhancedSquares.count() > 1) {
+        val numberOfBlocks = enhancedSquares.count()
+        val squareSide = if (numberOfBlocks % 2 == 0) 2 else 3
+
+        val x = mutableListOf<String>()
+        (0 until enhancedSquares.count() step squareSide)
+          .map { i -> enhancedSquares.subList(i, i + squareSide).map { it.split("/") } }
+          .forEach { thisBit ->
+              (0 until thisBit.first().count()).mapTo(x) { j -> thisBit.joinToString("") { it[j] } }
+          }
+        x.joinToString("/")
+    } else {
+        enhancedSquares.single()
+    }
+}
+
+
+private fun String.splitGrid(): List<String> {
+    val separator = determineSeparator()
+    val rows = this.split(separator)
+
+    if (rows.count() == 2 || rows.count() == 3) return listOf(this.convertToLine())
+
+//    println("rows $rows")
+
+    val gridSize = gridSize(rows)
+
+//    println("gridsize: $gridSize")
+
+    val x = mutableListOf<String>()
+    for (i in 0 until rows.count() step gridSize) {
+        val a = rows[i]
+        val b = rows[i + 1]
+
+        val c: String? = if (gridSize == 3) {
+            rows[i+2]
+        } else {
+            null
+        }
+
+        (0 until a.count() step gridSize).mapTo(x) {
+            var part = a.substring(it, it + gridSize) + "/" + b.substring(it, it + gridSize)
+
+            if (c != null) {
+                part += "/" + c.substring(it, it + gridSize)
+            }
+
+            part
+        }
+    }
+    return x.toList()
+}
+
+private fun gridSize(rows: List<String>): Int {
+    val rowCount = rows[0].count()
+    if (rowCount % 2 == 0) return 2
+    if (rowCount % 3 == 0) return 3
+    throw Exception("wait, what?! $rowCount")
+}
+
+private fun String.rotate(): String {
+    val separator = determineSeparator()
+
+    val rows = this.split(separator)
+
+    val rotated = mutableListOf<List<String>>()
+    rows.first().forEachIndexed { index, _ ->
+        val cols = rows.map { it[index].toString() }
+        rotated += cols.reversed()
+    }
+    return rotated.joinToString(separator) { it.joinToString("") }
+}
+
+private fun String.determineSeparator() = if (this.contains('/')) "/" else "\n"
+
+private fun String.generateRotatedGrids(): List<String> {
+    val s = this.convertFromLine()
+    val unflippedOptions = s.allRotations()
+    val flippedOptions = s.split("\n").joinToString("\n") { it.reversed() }.allRotations()
+    return unflippedOptions + flippedOptions
+}
+
+private fun String.replaceFrom(patterns: Map<String, String>): String {
+//    println("orig: $this")
+    val rotatedGrids = this.generateRotatedGrids()
+    val matched = rotatedGrids.find { patterns.containsKey(it) }
+    return if (matched != null) {
+        val match = patterns[matched]!!
+//        println("enhanced: $match")
+        match
+    } else {
+        this
+    }
+}
+
+private fun String.allRotations(): List<String> {
+    val results = mutableListOf<String>()
+    var rotatedString = this
+
+    repeat(4) {
+        rotatedString = rotatedString.rotate()
+        results.add(rotatedString)
+    }
+    return results.map(String::convertToLine)
+}
+
+private fun String.convertToLine(): String = this.replace('\n', '/')
+private fun String.convertFromLine(): String = this.replace('/', '\n')
+
+
